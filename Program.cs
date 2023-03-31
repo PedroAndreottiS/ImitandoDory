b4714d283437469bb31b1db19253fef23e65cd6d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-03-31 19:59:22 -0300

    Trabalho concluido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Escreva uma frase...");
+String frase = Console.ReadLine()!
+                    .Replace("o", "ooo")
+                    .Replace("a","aaa")
+                    .Replace("e","eee")
+                    .Replace("u","uuu")
+                    .Replace("i","iii")
+                    .Replace("O","OOO")
+                    .Replace("A","AAA")
+                    .Replace("E","EEE")
+                    .Replace("I","III")
+                    .Replace("U","UUU");
+Console.WriteLine(frase);
+Console.WriteLine("Parabéns! Agora você fala em baleiês!!!");
+
+
